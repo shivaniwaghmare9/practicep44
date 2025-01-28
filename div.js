db5c6069774b0e26b19120850b7196a56991e532
shivani <shivaniw69@gmail.com>
2025-01-29 01:06:25 +0530

    divworkhtml

--- div.js
+++ div.js
@@ -28,16 +28,16 @@ let tbl=[
 
 ]
 
-let show1=document.querySelector("#show")
+let show1=document.querySelector("#div")
  tbl.map((e)=>{
     show1.innerHTML+=
     `
     <div id="element">
     <h1 id="name">${e.name}</h1>
-    <h1 id="age">${e.age}</h1>
-    <h1 id="city">${e.city}</h1>
-    <h1 id="crs">${e.course}</h1>
-    <h1 id="uni">${e.univercity}</h1>
+    <h2 id="age">${e.age}</h2>
+    <h2 id="city">${e.city}</h2>
+    <h2 id="crs">${e.course}</h2>
+    <h2 id="uni">${e.univercity}</h2>
     </div>
 
     `
