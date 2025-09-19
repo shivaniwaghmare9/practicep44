ae75e353e6a1d0bb2431206c43debc691f0eecc7
shivani <shivaniw69@gmail.com>
2025-09-20 00:55:26 +0530

    js

--- question.3.js
+++ question.3.js
@@ -1,5 +1,47 @@
 
 
+// /*function green(){
+//     let box=document.querySelector("#box")
+//     box.style.backgroundColor="green";
+// }
+// function or(){
+//     let box=document.querySelector("#box")
+//     box.style.backgroundColor="orange";
+// }*/
+
+   
+//   function img1(){
+//     let changeimg= document.querySelector("#box");
+//     changeimg.style.backgroundImage="url('ural-owl.jpg')";
+//   }
+//    function img2(){
+//     let changeimg= document.querySelector("#box");
+//     changeimg.style.backgroundImage="url('baya-weaver.webp')";
+//    }
+//    function img3(){
+//     let changeimg= document.querySelector("#box");
+//     changeimg.style.backgroundImage="url('flycatcher.webp')";
+//    }
+//    function img4(){
+
+//     let changeimg= document.querySelector("#box");
+//     changeimg.style.backgroundImage="none";
+
+
+    
+//    }
+//    function img5(){
+//     let changeimg= document.querySelector("#box");
+//     changeimg.style.backgroundImage="none";
+//    }
+//    function img6(){
+//     let changeimg= document.querySelector("#box");
+//     changeimg.style.backgroundImage="none";
+//    }
+  
+
+
+
 /*function green(){
     let box=document.querySelector("#box")
     box.style.backgroundColor="green";
