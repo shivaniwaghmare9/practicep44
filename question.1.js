73c9fbc44536daf201592db91f7b0ac3c1a24de8
shivani <shivaniw69@gmail.com>
2025-09-20 00:50:54 +0530

    js

--- question.1.js
+++ question.1.js
@@ -1,3 +1,20 @@
+// /*function shiv(){
+//     const num=[3,7,1,9,2];
+//           ans=num.sort();
+//     console.log(num.reverse(ans));
+// }
+// //shiv();*/
+
+
+
+// let sub=()=>{
+//     let  inptext=document.querySelector("#inp").value
+   
+   
+//      document.body.style.backgroundColor=inptext;
+    
+   
+//   }
 /*function shiv(){
     const num=[3,7,1,9,2];
           ans=num.sort();
@@ -15,3 +32,4 @@ let sub=()=>{
     
    
   }
+  
