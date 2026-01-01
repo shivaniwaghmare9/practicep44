b99b7a85af9fa2489e2d022550282277b4e79aab
shivani <shivaniw69@gmail.com>
2026-01-01 21:50:58 +0530

    js

--- question.5.js
+++ question.5.js
@@ -1,3 +1,20 @@
+//  /*function shiv(){
+//     let ans="JavaScript is fun, but javaScript can be challenging."
+//     console.log(ans.replace(/JavaScript/gi, "Programming"))
+// }*/
+
+//  let ram=()=>{
+//     let change= document.querySelector("#btn")
+//     let change1= document.querySelector("#new")
+//     if(change.style.display==='none'){
+//       change.style.display='block';
+//        change1.innerHTML='Hide';
+//     }
+//     else{
+//        change.style.display='none';
+//        change1.innerHTML='Show';
+//     }
+// }
  /*function shiv(){
     let ans="JavaScript is fun, but javaScript can be challenging."
     console.log(ans.replace(/JavaScript/gi, "Programming"))
