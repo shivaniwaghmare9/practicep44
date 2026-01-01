4745e8486cf520bd262698d0e9e4f4b865409a3c
shivani <shivaniw69@gmail.com>
2026-01-01 21:58:05 +0530

    js

--- question.2.js
+++ question.2.js
@@ -1,3 +1,17 @@
+// function shiv(){
+//     let ans=confirm("Do you want to see the numbers from 1 to 10?")
+//     if(ans) {
+//         for(let i=1; i<=10; i++)
+        
+//             alert(i);
+        
+        
+//     }
+//     else{
+//         alert("you cancelled the action")
+//     }
+// }
+// //shiv();
 function shiv(){
     let ans=confirm("Do you want to see the numbers from 1 to 10?")
     if(ans) {
